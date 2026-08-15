94dac417041ac2715a11e41c3414d6e69ec44700
Peter Arevalo <pete.arevalo@icloud.com>
2026-08-14 19:13:18 -0500

    Add Vercel Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -44,6 +45,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
